51f02aad88989d27e075c7d3019f8ba3aa3d6644
Dandy Candra Oktaviano <dandycandra13@gmail.com>
2022-09-06 09:26:55 +0700

    Menambah lowongan

--- app/src/main/java/com/android/tubes_pbp/entity/lowongan.kt
+++ app/src/main/java/com/android/tubes_pbp/entity/lowongan.kt
@@ -1,4 +1,12 @@
 package com.android.tubes_pbp.entity
 
-class lowongan {
+import com.android.tubes_pbp.R
+
+class lowongan(var name: String, var detail: String, var photo: Int) {
+    companion object{
+        var listOfLowongan = arrayOf(
+            lowongan("Fedelis Brian", "Pengajar Kelas A, B , dan D", R.drawable.),
+        )
+
+    }
 }
