5b0ed7472e1d69b658b20ec716d762abec31737e
Jyestha <jyestha47@gmail.com>
2022-09-06 09:48:26 +0700

    menambah array di lowongan

--- app/src/main/java/com/android/tubes_pbp/entity/lowongan.kt
+++ app/src/main/java/com/android/tubes_pbp/entity/lowongan.kt
@@ -10,6 +10,11 @@ class lowongan(var name: String, var detail: String, var photo: Int) {
             lowongan("Software Developer", "Bisa bekerja dalam shift ,Min. Pendidikan S1 Teknik Informatika", R.drawable.software_developer),
             lowongan("Web Development", "Bisa bekerja Full Time ,Min. Pendidikan SMA/SMK/Sederajat", R.drawable.web_development),
             lowongan("UI / UX Design ", "Bisa bekerja dalam shift ,Min. Pendidikan S1 Teknik Informatika", R.drawable.ui_ux),
+            lowongan("Database Administrator","Bisa menggunakan MS Access, SQL Server, Min. Pendidikan S1 Teknik Informatika", R.drawable.database_administrator),
+            lowongan("Security Engineer","Menguasai tentang keamanan sistem, Min. Pendidikan S1 Teknik Informatika", R.drawable.cyber_security_engineer),
+            lowongan("System Analyst", "Memiliki analisa dan basic programing yang kuat, Min. Pendidikan S1 Teknik Informatika", R.drawable.system_analyst),
+            lowongan("Hardware Engineer", "Memiliki skill dalam arsitektur komputer, Min. Pendidikan SMA/SMK/Sederajat", R.drawable.computer_hardware_engineer),
+            lowongan("Network Engineer", "Memahami OSI layer, Min. Pendidikan S1 Teknik Informatika", R.drawable.network_enginer),
             )
 
     }
