23cfd7b423f7e1f0a548d0d2d34b60a9850bff09
Lyto78 <fransiscodiky@gmail.com>
2022-09-06 09:37:21 +0700

    Penambahan data untuk file Lowongan

--- app/src/main/java/com/android/tubes_pbp/entity/lowongan.kt
+++ app/src/main/java/com/android/tubes_pbp/entity/lowongan.kt
@@ -6,8 +6,11 @@ class lowongan(var name: String, var detail: String, var photo: Int) {
     companion object{
         var listOfLowongan = arrayOf(
             lowongan("Mobile Game Development", "Bisa bekerja dalam shift ,Min. Pendidikan S1 Teknik Informatika", R.drawable.game_development),
-
-        )
+            lowongan("Mobile App Development", "Bisa bekerja FUll Time ,Min. Pendidikan S1 Teknik Informatika", R.drawable.mobile_app_developer),
+            lowongan("Software Developer", "Bisa bekerja dalam shift ,Min. Pendidikan S1 Teknik Informatika", R.drawable.software_developer),
+            lowongan("Web Development", "Bisa bekerja Full Time ,Min. Pendidikan SMA/SMK/Sederajat", R.drawable.web_development),
+            lowongan("UI / UX Design ", "Bisa bekerja dalam shift ,Min. Pendidikan S1 Teknik Informatika", R.drawable.ui_ux),
+            )
 
     }
 }
