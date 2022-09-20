39b0afbe30cf0d030c9bc4cebe68592e446478fa
Dandy Candra Oktaviano <dandycandra13@gmail.com>
2022-09-20 09:26:55 +0700

    Migrasi fragment ke activity pada login dan register

---
+++ app/src/main/java/com/android/tubes_pbp/LoginActivity.kt
@@ -0,0 +1,54 @@
+package com.android.tubes_pbp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.tubes_pbp.databinding.ActivityLoginBinding
+import com.android.tubes_pbp.databinding.ActivityRegisterBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var lBundle : Bundle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()?.hide()
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        if(intent.getBundleExtra("registerBundle")!=null){
+            println("masukkk")
+            lBundle = intent.getBundleExtra("registerBundle")!!
+            binding.inputUsername.setText(lBundle.getString("username"))
+            binding.inputPassword.setText(lBundle.getString("password"))
+        }
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener {
+            if((binding.inputUsername.text.toString() == "admin" && binding.inputPassword.text.toString() == "admin")){
+                val moveHome = Intent(this, HomeActivity::class.java)
+                startActivity(moveHome)
+            }else{
+                if(binding.inputUsername.text.toString().isEmpty()){
+                    binding.layoutUsername.setError("Username Harus Diisi")
+                }else{
+                    binding.layoutUsername.setError("Username Salah")
+                }
+
+                if(binding.inputPassword.text.toString().isEmpty()){
+                    binding.layoutPassword.setError("Password Harus Diisi")
+                }else{
+                    binding.layoutPassword.setError("Password Salah")
+                }
+
+
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/android/tubes_pbp/RegisterActivity.kt
@@ -0,0 +1,81 @@
+package com.android.tubes_pbp
+
+import android.app.DatePickerDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.tubes_pbp.databinding.ActivityRegisterBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import java.util.*
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()?.hide()
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val cal = Calendar.getInstance()
+        val myYear = cal.get(Calendar.YEAR)
+        val myMonth = cal.get(Calendar.MONTH)
+        val myDay = cal.get(Calendar.DAY_OF_MONTH)
+
+        val intent = Intent(this, LoginActivity::class.java)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(intent)
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            if (!binding.inputUsername.text.toString().isEmpty() && !binding.inputPassword.text.toString().isEmpty() && !binding.inputTanggalLahir.text.toString().isEmpty() &&
+                !binding.inputEmail.text.toString().isEmpty() && !binding.inputNomorTelepon.text.toString().isEmpty()){
+                val bundle = Bundle()
+                bundle.putString("username",binding.inputUsername.text.toString())
+                bundle.putString("password",binding.inputPassword.text.toString())
+                intent.putExtra("registerBundle",bundle)
+                startActivity(intent)
+            } else {
+                if (binding.inputUsername.text.toString().isEmpty()){
+                    binding.layoutUsername.setError("Username Harus Diisi")
+                }
+                if (binding.inputPassword.text.toString().isEmpty()){
+                    binding.layoutPassword.setError("Password Harus Diisi")
+                }
+                if (binding.inputTanggalLahir.text.toString().isEmpty()){
+                    binding.layoutTanggalLahir.setError("Tanggal Lahir Harus Diiisi")
+                }
+                if (binding.inputEmail.text.toString().isEmpty()){
+                    binding.layoutEmail.setError("Email Harus Diisi")
+                }
+                if (binding.inputNomorTelepon.text.toString().isEmpty()){
+                    binding.layoutNomorTelepon.setError("Nomor Telp Harus Diiisi")
+                }
+            }
+        }
+
+        binding.inputTanggalLahir.setOnFocusChangeListener { view, b ->
+            val datePicker =
+                this?.let { it1 ->
+                    DatePickerDialog(it1, DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
+                        binding.inputTanggalLahir.setText("${dayOfMonth}/${(month.toInt() + 1).toString()}/${year}")
+                    },myYear,myMonth,myDay)
+                }
+            if(b){
+                datePicker?.show()
+            }else{
+                datePicker?.hide()
+            }
+
+        }
+
+
+
+    }
+
+}
