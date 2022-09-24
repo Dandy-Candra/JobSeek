08c7fe79abad0cbb4736b97741aaae9289e7bb04
Dandy Candra Oktaviano <dandycandra13@gmail.com>
2022-09-24 10:26:11 +0700

    Notifikasi saat register berhasil

--- app/src/main/java/com/android/tubes_pbp/NotificationReceiver.kt
+++ app/src/main/java/com/android/tubes_pbp/NotificationReceiver.kt
@@ -5,15 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
-<<<<<<< HEAD
+
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent) {
         val message = intent.getStringExtra("toastMessage")
-=======
-class NotificationReceiver : BroadcastReceiver(){
-    override fun onReceive(context: Context?, intent: Intent) {
-        val message = intent.getStringExtra("ToastMessage")
->>>>>>> origin/master
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
