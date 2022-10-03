09ffee6c71cf1919fd6dfa5d33b3eb76f721c55d
Dandy Candra Oktaviano <dandycandra13@gmail.com>
2022-10-03 22:22:01 +0700

    Menambah notif

--- app/src/main/java/com/android/tubes_pbp/DetailLowongan.kt
+++ app/src/main/java/com/android/tubes_pbp/DetailLowongan.kt
@@ -1,22 +1,83 @@
 package com.android.tubes_pbp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.android.tubes_pbp.databinding.ActivityDetailLowonganBinding
 import com.android.tubes_pbp.databinding.ActivityRegisterBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class DetailLowongan : AppCompatActivity() {
     private lateinit var binding: ActivityDetailLowonganBinding
+    private val CHANNEL_ID_SAVE = "channel_notification_save"
+    private val notificationId = 103
     override fun onCreate(savedInstanceState: Bundle?) {
         getSupportActionBar()?.hide()
         super.onCreate(savedInstanceState)
         binding = ActivityDetailLowonganBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        createNotificationChannel()
 
         val bundle = intent.getBundleExtra("detailLowongan")
         binding.title.setText(bundle?.getString("title",""))
         binding.description.setText(bundle?.getString("description",""))
         binding.image.setImageResource(bundle?.getInt("photo",0)!!)
 
+        binding.btnLamar.setOnClickListener {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Apakah Anda Ingin Melamar ?")
+                    .setNegativeButton("No") { dialog, which ->
+                        // Respond to negative button press
+                    }
+                    .setPositiveButton("yes") { dialog, which ->
+                        sendNotification2(binding.title.text.toString(),binding.description.text.toString())
+                    }
+                    .show()
+
+        }
+
     }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Save"
+            val descriptionText = "Notification Description"
+
+            val channel1 = NotificationChannel(CHANNEL_ID_SAVE,name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+
+            val notificationManager: NotificationManager =
+                this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+        }
+    }
+
+    private fun sendNotification2(title : String, description : String){
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID_SAVE)
+            .setSmallIcon(R.drawable.ic_baseline_info_24)
+            .setContentTitle("Berhasil Menyimpan Experience!")
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setColor(Color.BLUE)
+            .setStyle(
+                NotificationCompat.BigTextStyle().bigText("Deskripsi : "+description)
+                    .setBigContentTitle("Nama Perusahaan : "+title)
+                    .setSummaryText("Hiring Succes"))
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId, builder.build())
+        }
+    }
+
+
+
 }
